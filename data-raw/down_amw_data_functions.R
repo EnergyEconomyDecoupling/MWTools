0c14680165c56304ed8ab6a180e0e9c2f6bd558e
ZekeMarshall <marshall_zeke@outlook.com>
2021-05-04 17:00:47 +0100

    Bundling raw data

--- data-raw/down_amw_data_functions.R
+++ data-raw/down_amw_data_functions.R
@@ -1,29 +1,16 @@
-#' Download live animals data from FAOSTAT
-#'
-#' This function uses the bulk download facility function from the R package
-#' `FAOSTAT` to download data for the number of live animals by country over time.
-#'
-#' @param data_folder A user-specified file path to the directory in which to
-#'                    download the FAO live animals data.
-#' @param live_animals_code See `MWTools::fao_codes$live_animals_code`.
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' live_animals_data <- down_fao_live_animals(data_folder = file.path(fs::home_path(), "FAO_data"))
-#'
-down_fao_live_animals <- function(download_location = MWTools::extdata_path(),
-                                  live_animals_code = MWTools::fao_codes$live_animals_code){
+# This script uses the R package FAOSTAT to download data for the number of live
+# animals in the form of a
 
-  # Download .zip file containing data for the number of live animals into a specified folder
-  amw_data <- FAOSTAT::get_faostat_bulk(code = live_animals_code, data_folder = download_location)
+# Establish constants
+download_location <- tempdir()
+live_animals_code <- MWTools::fao_codes$live_animals_code
+download_filename <- "Production_Livestock_E_All_Data_(Normalized).zip"
 
-  write.csv(x = amw_data, file = file.path(download_location, "amw_data.csv"))
+# Download .zip file containing data for the number of live animals into a specified folder
+fao_amw_data <- FAOSTAT::get_faostat_bulk(code = live_animals_code, data_folder = download_location)
 
-  }
+# Use amw data in package
+usethis::use_data(fao_amw_data, overwrite = TRUE)
 
-# Call down_fao_live_animals() function, activated when sourcing down_amw_data_functions.R
-down_fao_live_animals()
-
-
+# Remove file from temporary directory
+file.remove(file.path(download_location, download_filename))
--- data-raw/down_hmw_data_functions.R
+++ data-raw/down_hmw_data_functions.R
@@ -1,62 +1,45 @@
-################################################################################
-## ILO data ##
-################################################################################
+# This script uses the R package Rilostat to download data for the number of
+# employed persons and number of working hours, and then forms a data frame to
+# to bundle in this package.
 
-#' Retrieve ILO data for working hours and employed persons
-#'
-#' Use the function `get_ilostat` function from the `Rilostat` package to create a
-#' tidy data frame containing mean yearly working hours and number of employed persons
-#' by country, sex, sector, and year.
-#'
-#' @param country_name,sex_ilo_col,sector_col,year,yearly_working_hours_ilo_col,employed_persons_ilo_col See `MWtools::mw_constants()`.
-#' @param working_hours_code,employment_code See `MWTools::ilo_codes`.
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' ilo_hmw_data <- get_ilo_hmw_data()
-#'
-get_ilo_hmw_data <- function(country_code_col = MWTools::conc_cols$country_code_col,
-                             sex_ilo_col = MWTools::ilo_cols$sex_ilo_col,
-                             sector_col = MWTools::mw_constants$sector_col,
-                             year = MWTools::mw_constants$year,
-                             ref_area_col = MWTools::ilo_cols$ref_area_col,
-                             yearly_working_hours_ilo_col = MWTools::ilo_cols$yearly_working_hours_ilo_col,
-                             employed_persons_ilo_col = MWTools::ilo_cols$employed_persons_ilo_col,
-                             working_hours_code = MWTools::ilo_codes$working_hours_code,
-                             employment_code = MWTools::ilo_codes$employment_code,
-                             download_location = MWTools::extdata_path()
-                             ){
+# Establish constants
+country_code_col <- MWTools::conc_cols$country_code_col
+sex_ilo_col <- MWTools::ilo_cols$sex_ilo_col
+sector_col <- MWTools::mw_constants$sector_col
+year <- MWTools::mw_constants$year
+ref_area_col <- MWTools::ilo_cols$ref_area_col
+yearly_working_hours_ilo_col <- MWTools::ilo_cols$yearly_working_hours_ilo_col
+employed_persons_ilo_col <- MWTools::ilo_cols$employed_persons_ilo_col
+working_hours_code <- MWTools::ilo_codes$working_hours_code
+employment_code <- MWTools::ilo_codes$employment_code
+download_location <- MWTools::extdata_path()
 
-  # Mean weekly hours actually worked per employed person by sex and economic activity: HOW_TEMP_SEX_ECO_NB_A
-  working_hours <- Rilostat::get_ilostat(id = working_hours_code,
-                                         quiet = TRUE) %>%
-    Rilostat::label_ilostat(code = c(ref_area_col)) %>%
-    dplyr::select(ref_area, sex.label, classif1.label, time, obs_value) %>% # Create constants
-    magrittr::set_colnames(c(country_code_col, sex_ilo_col, sector_col, year, yearly_working_hours_ilo_col))
+# Mean weekly hours actually worked per employed person by sex and economic activity:
+# HOW_TEMP_SEX_ECO_NB_A
+working_hours <- Rilostat::get_ilostat(id = working_hours_code,
+                                       quiet = TRUE) %>%
+  Rilostat::label_ilostat(code = c(ref_area_col)) %>%
+  dplyr::select(ref_area, sex.label, classif1.label, time, obs_value) %>% # Create constants
+  magrittr::set_colnames(c(country_code_col, sex_ilo_col, sector_col, year, yearly_working_hours_ilo_col))
 
-  # Employment by sex and economic activity (thousands): EMP_TEMP_SEX_ECO_NB_A
-  employment <- Rilostat::get_ilostat(id = employment_code,
-                                      quiet = TRUE) %>%
-    Rilostat::label_ilostat(code = c(ref_area_col)) %>%
-    dplyr::select(ref_area, sex.label, classif1.label, time, obs_value) %>%
-    magrittr::set_colnames(c(country_code_col, sex_ilo_col, sector_col, year, employed_persons_ilo_col))
+# Employment by sex and economic activity (thousands): EMP_TEMP_SEX_ECO_NB_A
+employment <- Rilostat::get_ilostat(id = employment_code,
+                                    quiet = TRUE) %>%
+  Rilostat::label_ilostat(code = c(ref_area_col)) %>%
+  dplyr::select(ref_area, sex.label, classif1.label, time, obs_value) %>%
+  magrittr::set_colnames(c(country_code_col, sex_ilo_col, sector_col, year, employed_persons_ilo_col))
 
-  # Convert Employed persons [1000 persons] to employed persons [persons] and
-  # mean working hours [hours/week] to mean working hours [hours/year]
-  ilo_hmw_data <- employment %>%
-    dplyr::left_join(working_hours, by = c(country_code_col, sex_ilo_col, sector_col, year)) %>%
-    dplyr::mutate(
-      "{employed_persons_ilo_col}" := .data[[employed_persons_ilo_col]] * 1000
-    ) %>%
-    dplyr::mutate(
-      "{yearly_working_hours_ilo_col}" := .data[[yearly_working_hours_ilo_col]] * 52
-    )
+# Convert Employed persons [1000 persons] to employed persons [persons] and
+# mean working hours [hours/week] to mean working hours [hours/year]
+ilo_hmw_data <- employment %>%
+  dplyr::left_join(working_hours, by = c(country_code_col, sex_ilo_col, sector_col, year)) %>%
+  dplyr::mutate(
+    "{employed_persons_ilo_col}" := .data[[employed_persons_ilo_col]] * 1000
+  ) %>%
+  dplyr::mutate(
+    "{yearly_working_hours_ilo_col}" := .data[[yearly_working_hours_ilo_col]] * 52
+  )
 
-  write.csv(ilo_hmw_data, file = file.path(download_location, "ilo_hmw_data.csv"))
+# Use hmw data in package
+usethis::use_data(ilo_hmw_data, overwrite = TRUE)
 
-}
-
-# Call get_ilo_hmw_data() function, activated when sourcing down_hmw_data_functions.R
-get_ilo_hmw_data()
