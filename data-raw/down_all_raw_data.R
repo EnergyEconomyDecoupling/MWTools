2011e05e582078c1bf8b5da1a5ac5fa9c0fbb42f
Matthew Heun <matthew.heun@me.com>
2023-08-10 12:34:56 -0400

    Edit the download script.

--- data-raw/down_all_raw_data.R
+++ data-raw/down_all_raw_data.R
@@ -5,16 +5,17 @@
 library(FAOSTAT)
 library(Rilostat)
 
-# Establish the version folder to save the raw data in
+# Establish the version folder in which to save the raw data
 version <- "v1.2"
 
 # Establish FAO constants
 download_location <- tempdir()
 live_animals_code <- "QCL"
-download_filename <- ""
+# download_filename <- ""
 
 # Download .zip file containing data for the number of live animals into a specified folder
-fao_amw_data <- FAOSTAT::get_faostat_bulk(code = live_animals_code, data_folder = download_location)
+fao_amw_data <- FAOSTAT::get_faostat_bulk(code = live_animals_code,
+                                          data_folder = download_location)
 
 # Save downloaded file
 saveRDS(object = fao_amw_data,
@@ -25,7 +26,7 @@ saveRDS(object = fao_amw_data,
 working_hours_code <- MWTools::ilo_codes$working_hours_code
 employment_code <- MWTools::ilo_codes$employment_code
 
-ilo_workingHours_data <- Rilostat::get_ilostat(id = working_hours_code,
+ilo_working_hours_data <- Rilostat::get_ilostat(id = working_hours_code,
                                                quiet = TRUE) |>
   Rilostat::label_ilostat(code = c("ref_area"))
 
@@ -33,8 +34,8 @@ ilo_employment_data <- Rilostat::get_ilostat(id = employment_code,
                                              quiet = TRUE) |>
   Rilostat::label_ilostat(code = c("ref_area"))
 
-saveRDS(object = ilo_workingHours_data,
+saveRDS(object = ilo_employment_data,
         file = PFUSetup::get_abs_paths(version = version)$ilo_employment_data_path)
 
-saveRDS(object = ilo_employment_data,
-        file = PFUSetup::get_abs_paths(version = version)$ilo_workingHours_data_path)
+saveRDS(object = ilo_working_hours_data,
+        file = PFUSetup::get_abs_paths(version = version)$ilo_working_hours_data_path)
